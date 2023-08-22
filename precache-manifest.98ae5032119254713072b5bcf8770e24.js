cfc579ba95212145b1f096bef7673f75d635e5c4
negin <neginfarahmand69@gmail.com>
2023-08-22 20:08:33 +1000

    Updates

---
+++ precache-manifest.98ae5032119254713072b5bcf8770e24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "34dfcbd5c8b1f648291d",
+    "url": "/Portfolio/static/css/main.b7db3a1b.chunk.css"
+  },
+  {
+    "revision": "34dfcbd5c8b1f648291d",
+    "url": "/Portfolio/static/js/main.34dfcbd5.chunk.js"
+  },
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.591eb5be.js"
+  },
+  {
+    "revision": "e325d553b176b4223d78",
+    "url": "/Portfolio/static/js/2.e325d553.chunk.js"
+  },
+  {
+    "revision": "28cb36065bd4f5b7eda84b65fa230c5d",
+    "url": "/Portfolio/static/media/p.28cb3606.jpg"
+  },
+  {
+    "revision": "ab422e11270715c27fd0a0dae1c35103",
+    "url": "/Portfolio/index.html"
+  }
+];
